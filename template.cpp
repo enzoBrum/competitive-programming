7f3ecf826fd48c3a916b7588fdf0dc0ee983f953
Enzo Brum <darosabrumenzo@gmail.com>
2024-05-16 23:21:54 -0300

    add template

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+#ifdef DEBUG
+#define PRINT(s) std::cout << s << '\n';
+#endif
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+}
