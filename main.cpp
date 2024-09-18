952e8a926831c453387fdf74f8318c9d202520e4
Enzo Brum <darosabrumenzo@gmail.com>
2024-09-18 16:35:41 -0300

    idk

--- main.cpp
+++ main.cpp
@@ -4,56 +4,51 @@ using namespace std;
 using pii = pair<int, int>;
 using ll = long long;
 using ull = unsigned long long;
-using pli = pair<ll, int>;
 
+bool used[10];
+int n;
+vector<string> dances;
 
-ll dfs(int u, ll power, vector<vector<pli>>& adj) {
-  ll ret = power;
+int backtrack(int depth, int prev) {
+  if (depth == n)
+    return 0;
 
-  for (auto&[p, v] : adj[u])
-    ret = max(ret, dfs(v, power + p, adj));
-  return ret;
+  int best = 0;
+  for (int i = 0; i < n; ++i) {
+    if (used[i])
+      continue;
+    used[i] = true;
+
+    int count[26];
+    memset(count, 0, sizeof(count));
+    int cur = 0;
+    if (depth > 0) {
+      for (auto &c : dances[prev])
+        count[c - 'A']++;
+      for (auto &c : dances[i])
+        count[c - 'A']++;
+      for (int i = 0; i < 26; ++i)
+        if (count[i] == 2)
+          cur++;
+    }
+    cur += backtrack(depth + 1, i);
+    best = min(best, cur);
+    used[i] = false;
+  }
+  return best;
 }
-
 
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
-  int n;
+
   cin >> n;
 
-  vector<pair<string, ll>> vec(n);
-  for (auto&[s, p] : vec)
-    cin >> s >> p;
-  sort(vec.begin(), vec.end());
-  
-  vector<vector<pair<ll, int>>> adj(n);
-  vector<int> in_degree(n);
-  
-  for (int i = 0; i < n; ++i) {
-    for (int j = i+1; j < n; ++j) {
-      if (vec[j].first.size() <= vec[i].first.size())
-        break;
-      
-      bool proper = true;
-      for (int k = 0; k < vec[i].first.size(); ++k)
-        proper = proper && vec[i].first[k] == vec[j].first[k];
+  memset(used, 0, sizeof(used));
 
-      if (proper) {
-        adj[i].push_back({vec[j].second, j});
-        in_degree[j]++;
-      }
-    }
-  }
+  dances.resize(n);
+  for (auto &v : dances)
+    cin >> v;
 
-  ll ans = 0;
-  for (int i = 0; i < n; ++i) {
-    if (in_degree[i])
-      continue;
-
-    ans = max(ans, dfs(i, vec[i].second, adj));
-  }
-  
-  cout << ans << '\n';
+  cout << backtrack(0, -1) << '\n';
 }
